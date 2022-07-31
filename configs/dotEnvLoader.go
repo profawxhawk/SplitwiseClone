5f9ec6f6820302be678db7f4efd481da51b074ff
profawxhawk <bharath.thulasidoss@zomato.com>
2022-07-31 16:52:27 +0530

    adds configuration to load from env

---
+++ configs/dotEnvLoader.go
@@ -0,0 +1,18 @@
+package configs
+
+import (
+	"github.com/joho/godotenv"
+	"log"
+	"os"
+)
+
+func getEnvWithKey(key string) string {
+	return os.Getenv(key)
+}
+
+func LoadEnv() {
+	err := godotenv.Load("../.env")
+	if err != nil {
+		log.Fatal("Error loading env file")
+	}
+}
